a9dc6790eedb7f5fb73df94044c4165dda8fd67e
akhad <akhadyulchiev@gmail.com>
2022-12-04 23:42:19 +0500

    implement rds

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,14 +1,32 @@
 import { Module } from '@nestjs/common';
+import { Client } from 'pg';
 
 import { OrderModule } from '../order/order.module';
 
 import { CartController } from './cart.controller';
 import { CartService } from './services';
+import { DB } from '../common/params';
 
+const client = new Client({
+  user: 'postgres',
+  host: DB.DB_HOST,
+  database: DB.DB_NAME,
+  password: DB.DB_PASSWORD,
+  port: 5432,
+});
+
+client.connect();
 
 @Module({
-  imports: [ OrderModule ],
-  providers: [ CartService ],
-  controllers: [ CartController ]
+  imports: [OrderModule],
+  providers: [
+    CartService,
+    {
+      provide: 'PG',
+      useValue: client,
+    },
+  ],
+  controllers: [CartController],
+  exports: ['PG'],
 })
 export class CartModule {}
---
+++ src/common/params/params.ts
@@ -0,0 +1,5 @@
+export const DB = {
+  DB_HOST: 'cloud-x-shop.c4t640xeavt0.eu-central-1.rds.amazonaws.com',
+  DB_NAME: 'cloud_x_shop',
+  DB_PASSWORD: 'postgres',
+};
